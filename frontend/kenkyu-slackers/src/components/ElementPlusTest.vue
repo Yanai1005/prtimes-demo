fcadebbafe9b7395f01faa658eaec1bcd08632dc
Malb7mm <ykmeak9g@gmail.com>
2025-09-08 17:23:20 +0900

    chore: installed element plus

---
+++ frontend/kenkyu-slackers/src/components/ElementPlusTest.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  const hoge = () => {
+    window.alert("Hoge!")
+  }
+</script>
+
+<template>
+  <el-button @click="hoge">Button!</el-button>
+</template>
+
+<style scoped></style>
